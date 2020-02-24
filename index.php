576ef9258eaa9cf755a7464ba1b93c3de1e17a35
Pilkapilka <juste.et.k@gmail.com>
2020-02-24 12:52:38 +0200

    New start

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-print 'Lenta | PHP Fight Club';
 date('Y-m-d H:i:s');
-print date('Y-m-d H:i:s');
+print date('Y-m-d H:i:s') .'Justė grįžo į PHPFIGHTCLUB';
 ?>
+
+<html>
+    <head>
+    <title><?php print 'Lenta | PHP Fight Club'; ?> </title>
+    </head>
+
+<body>
+    <p><?php print 'Ko žiūri?'; ?></p>
+
+</body>
+</html>
