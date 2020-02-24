579a94512243d6f7a92dc89c957cf353077f244c
Pilkapilka <juste.et.k@gmail.com>
2020-02-24 13:34:58 +0200

    New start

--- index.php
+++ index.php
@@ -5,13 +5,26 @@ $n= rand(2, 1000);
 
 ?>
 
+
 <html>
 <head>
-    <title><?php print 'Facts | PHP Fight Club'; ?> </title>
+    <title><?php print 'Coin-Flip'; ?> </title>
+    <style>
+        .coin_heads{
+            background-image:url('<?php; ?>');
+        }
+
+        .coin_tails{
+            background-image: url('<?php');
+        }
+    </style>
 </head>
 
 <body>
-<p>Per šią sekundę organizmas pagamino <?php print $n; ?>mil. kraujo ląstelių.</p>
+    <div class="coin_heads coin_tails">
+    <span>
+    </span>
+    </div>
 
 </body>
 </html>
