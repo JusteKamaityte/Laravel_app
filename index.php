42a373493432471dc09d06f1677d176be3f1f4d9
Pilkapilka <juste.et.k@gmail.com>
2020-02-25 10:12:55 +0200

    task #5

--- index.php
+++ index.php
@@ -1,31 +1,55 @@
 <?php
-$date= date('Y-m-d H:i:s');
-$vardas= "Justė";
-$n= rand(2, 1000);
+$title = 'Coin-flip';
+$x = rand(0, 2);
+$class= 0;
+$span= 0;
 
+if ($x == 1) {
+    $class='heads';
+} else {
+    $class='tails';
+}
+
+$span = $class;
 ?>
 
 
 <html>
 <head>
-    <title><?php print 'Coin-Flip'; ?> </title>
+    <meta charset="utf-8">
+    <title><?php print $title ?></title>
     <style>
-        .coin_heads{
-            background-image:url('<?php; ?>');
+
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        span {
+            text-transform: capitalize;
+            font-size: 50px;
+            font-weight: bold;
+        }
+        div{
+            height: 500px;
+            width: 500px;
+            background-size:cover;
+        }
+        .heads{
+            background-image:url("https://www.leftovercurrency.com/wp-content/uploads/2016/11/1-american-dollar-coin-reverse-1.jpg");
+        }
+        .tails{
+            background-image: url("https://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg");
         }
 
-        .coin_tails{
-            background-image: url('<?php');
-        }
     </style>
+
 </head>
 
 <body>
-    <div class="coin_heads coin_tails">
-    <span>
-    </span>
-    </div>
-
+    <div class="<?php print $class ?>"></div>
+        <span><?php print $span ?></span>
 </body>
 </html>
 
