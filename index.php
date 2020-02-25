cbb12d73748940daaa78ffc4ac8d12856874e161
Pilkapilka <juste.et.k@gmail.com>
2020-02-25 10:19:52 +0200

    task #6

--- index.php
+++ index.php
@@ -1,55 +1,29 @@
 <?php
-$title = 'Coin-flip';
-$x = rand(0, 2);
-$class= 0;
-$span= 0;
+$title = 'Sriuba';
+$soup_ml = rand(400,700);
+$soup_temp = rand(15, 40);
+$piss_ml = rand(100,350);
+$piss_temp = 36.4;
 
-if ($x == 1) {
-    $class='heads';
-} else {
-    $class='tails';
-}
+$soup_piss_temp = round(($soup_ml * $soup_temp + $piss_ml * $piss_temp) / ($soup_ml + $piss_ml), 1);
 
-$span = $class;
+$h1 = 'Sriubos prognoze';
+$p1 = "Pradzioje puode buv $soup_ml ml. $soup_temp C. sriubos";
+$p2 = "I puoda primyzus $piss_ml ml., sriubos temperatura patapo $soup_piss_temp C.";
+
 ?>
+
 
 
 <html>
 <head>
     <meta charset="utf-8">
-    <title><?php print $title ?></title>
-    <style>
-
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
-        span {
-            text-transform: capitalize;
-            font-size: 50px;
-            font-weight: bold;
-        }
-        div{
-            height: 500px;
-            width: 500px;
-            background-size:cover;
-        }
-        .heads{
-            background-image:url("https://www.leftovercurrency.com/wp-content/uploads/2016/11/1-american-dollar-coin-reverse-1.jpg");
-        }
-        .tails{
-            background-image: url("https://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg");
-        }
-
-    </style>
-
+    <title><?php print $print_title?></title>
 </head>
-
 <body>
-    <div class="<?php print $class ?>"></div>
-        <span><?php print $span ?></span>
+    <h1><?php print $h1 ?></h1>
+    <p><?php print $p1 ?></p>
+    <p><?php print $p2 ?></p>
 </body>
 </html>
 
