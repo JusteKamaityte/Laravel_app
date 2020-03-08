baa87610d1f37b53cad013ec0960ea1250a145c6
Pilkapilka <juste.et.k@gmail.com>
2020-03-08 22:35:08 +0200

    for cycle

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ for($kisene = 100; $kisene > 0; $kisene--){
  if ($kisene <50){
      break;
  }
+// kiekvieno ciklo metu kišenėje bus vienu euru mažiau
+// ciklas vyks tol kol kišenė nebus tusčia: ($kisene>0)
 }
 ?>
 
