c534c4f0573a9421bd7175166da6d181d66a6e3d
Pilkapilka <juste.et.k@gmail.com>
2020-03-09 00:34:06 +0200

    Using the list function

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
-$paper = array('copier' => "Copier & Multipurpose",
-    'inkjet' => "Inkjet Printer",
-    'laser' => "Laser Printer",
-    'photo' => "Photographic Paper");
-while (list($item, $description) = each($paper))
-    echo "$item: $description<br>";
+list($a, $b) = array('Alice', 'Bob');
+echo "a=$a b=$b";
 ?>
+
 
 
 <html>
 <head>
     <title>CYCLES</title>
+<!--    You can see how list works a little more clearly in Example 6-9, where an array is created-->
+<!--    out of the two strings “Alice” and “Bob” and then passed to the list function, which-->
+<!--    assigns those strings as values to the variables $a and $b.-->
+
+<!--    You can take your pick when walking through arrays. Use foreach...as to create a loop-->
+<!--    that extracts values to the variable following the as, or use the each function and create-->
+<!--    your own looping system.-->
 
 </head>
 <body>
