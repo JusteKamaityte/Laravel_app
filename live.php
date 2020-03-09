a355f7a3311a371f01ad7a07e04792044d7a29bb
Pilkapilka <juste.et.k@gmail.com>
2020-03-09 13:32:55 +0200

    live page

--- live.php
+++ live.php
@@ -2,11 +2,41 @@
     $participants = [
         [
             'name' => 'Bimbis Pongas',
+            'bike'=> ' css/assets/image_1.jpg'
+        ],
+        [
+            'name' => 'Bimbis Pongas',
+            'bike'=> ' .jpg'
+        ],
+        [
+            'name' => 'Bimbis Pongas',
+            'bike'=> ' .jpg'
+        ],
+        [
+            'name' => 'Bimbis Pongas',
+            'bike'=> ' .jpg'
+        ],
+        [
+            'name' => 'Bimbis Pongas',
+            'bike'=> ' .jpg'
+        ],
+        [
+            'name' => 'Bimbis Pongas',
             'bike'=> ' .jpg'
         ]
-    ]
+    ];
+$leader= null;
 
-foreach ($participants as $participant)
+foreach ($participants as $participant){
+ $x = rand(0, 200);
+ $y = rand(0,200);
+//
+// if ($x=0 $x > 100) {
+//    $participant['name']
+// }
+}
+
+var_dump($participants);
 ?>
 
 
@@ -15,24 +45,16 @@ foreach ($participants as $participant)
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/main.css">
     <title>Team</title>
-
 </head>
 <body>
 <main>
     <nav></nav>
     <h1>Tour-De</h1>
-    <h2> Current Leader: </h2>
     <div class="wrapper">
-        <div class="cards">
-            <?php foreach ($cards as $card): ?>
-                <div class="card">
-                    <img src="<?php print $card ['photo']; ?>">
-                    <span> <?php print $card['stats']; ?></span>
-                    <span><?php print $card['full_name']; ?></span>
-                    <span><?php print $card['win_percentage']; ?> </span>
-                </div>
+            <?php foreach ($participants as $participant): ?>
+                <h2> Current Leader:<?php print $participant['name']; ?></h2>
+                <img src="<?php print $participants ['bike']; ?>">
             <?php endforeach; ?>
-        </div>
     </div>
 </main>
 </body>
