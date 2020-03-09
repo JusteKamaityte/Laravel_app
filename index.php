11604a5478dcbe14554b616261bc9290aab90f97
Pilkapilka <juste.et.k@gmail.com>
2020-03-09 11:23:13 +0200

    foreach loop

--- index.php
+++ index.php
@@ -1,48 +1,46 @@
 <?php
 $h1 = 'Participants';
-    $participants =[
-        [
-            'name' => 'Justė',
-            'last name' => 'Kamaitytė',
-            'photo' => 'assets/image.jpg'
-        ],
-        [
-            'name' => 'Algis',
-            'last name' => 'Algirdauskas',
-            'photo' => 'assets/image.jpg'
-        ],
-        [
-            'name' => 'Saulius',
-            'last name' => 'Bolauskas',
-            'photo' => 'assets/image.jpg'
-        ],
-        [
-            'name' => 'Petras',
-            'last name' => 'Petrauskas',
-            'photo' => 'assets/image.jpg'
-        ],
-        [
-            'name' => 'Tomas',
-            'last name' => 'Tomauskas',
-            'photo' => 'assets/image.jpg'
-        ],
-        [
-            'name' => 'Linas',
-            'last name' => 'Linauskas',
-            'photo' => 'assets/image.jpg'
-        ],
+
+    $participants = [
+
+        'names' => ['Justė', 'Algis', 'Saulius', 'Petras', 'Tomas', 'Linas'],
+        'last_names' => ['Kamaitytė', 'Algirdauskas', 'Bolauskas', 'Petrauskas', 'Tomauskas', 'Linauskas'],
+        'images' => ['css/assets/image_1.jpg', 'css/assets/image.jpg', 'assets/image.jpg', 'assets/image.jpg', 'assets/image.jpg', 'assets/image.jpg']
+//            result[],
+//            percentage[]
+
     ];
+
+//    for $participants
+//    foreach ($participants as $value)
+
+    $key = array_rand($participants);
+
+    var_dump($participants);
+
+    for ($participants = 0 ; $participants > 6 ; ++$participants)
+
 ?>
 
 
 
-<html>
+<html lang="en" dir="ltr">
 <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
     <title>Team</title>
 
 </head>
 <body>
-<h1>Participants</h1>
+<?php print $h1; ?>
+<div class="cards">
+    <?php foreach ($participants as $participant): ?>
+    <div class="card">
+        <img src="<?php print $participants[rand(0,3)]['images'] ?>">
+        <h2><?php print $participants[rand(0,3)]['names']; ?></h2>
+        <h2><?php print $participants[rand(0,3)]['last_names']; ?></h2>
+    </div>
+    <?php endforeach; ?>
+</div>
 </body>
 </html>
-
