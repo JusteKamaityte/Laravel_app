c1d36e9f82fee1c83f37ccc61baf95e59fc21235
Pilkapilka <juste.et.k@gmail.com>
2020-03-09 22:20:23 +0200

    live arena

--- live.php
+++ live.php
@@ -25,16 +25,16 @@
             'bike'=> ' .jpg'
         ]
     ];
-$leader= null;
-
-foreach ($participants as $participant){
- $x = rand(0, 200);
- $y = rand(0,200);
+//$leader= null;
+//
+//foreach ($participants as $participant){
+// $x = rand(0, 200);
+// $y = rand(0,200);
 //
 // if ($x=0 $x > 100) {
 //    $participant['name']
 // }
-}
+
 
 var_dump($participants);
 ?>
@@ -43,18 +43,36 @@ var_dump($participants);
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/live.css">
     <title>Team</title>
 </head>
 <body>
-<main>
+<main class="main_section">
     <nav></nav>
     <h1>Tour-De</h1>
     <div class="wrapper">
-            <?php foreach ($participants as $participant): ?>
-                <h2> Current Leader:<?php print $participant['name']; ?></h2>
-                <img src="<?php print $participants ['bike']; ?>">
-            <?php endforeach; ?>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px">
+<!--            --><?php //foreach ($participants as $participant): ?>
+<!--                <h2> Current Leader:--><?php //print $participant['name']; ?><!--</h2>-->
+<!--                <img src="--><?php //print $participants ['bike']; ?><!--">-->
+<!--            --><?php //endforeach; ?>
+        </div>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px" border="3px solid black">
+        </div>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px">
+        </div>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px">
+        </div>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px">
+        </div>
+        <div class="bike">
+            <img src="css/assets/bike1.png" width="100px" height="100px">
+        </div>
     </div>
 </main>
 </body>
