f7e80b792296d72755d0c8795d151cb7028351dd
Pilkapilka <juste.et.k@gmail.com>
2020-03-12 09:48:32 +0200

    uzduotis

--- index.php
+++ index.php
@@ -1,40 +1,19 @@
 <?php
-$nav = [];
+    $name =
 
-$h1 = 'Participants';
-
-
-$names = ['Algis', 'Saulius', 'Petras', 'Jonas', 'Linas', 'Tomas'];
-$last_names = ['Algirdauskas', 'Sauliauskas', 'Petrauskas', 'Jonauskas', 'Linauskas', 'Tomauskas'];
-$photos = ['css/assets/bike1.png', 'css/assets/bike2.png', 'css/assets/bike3.png', 'css/assets/bike4.png', 'css/assets/bike5.png', 'css/assets/bike6.png'];
-
-for ($i = 0; $i < 6; $i++) {
-    $name_key = array_rand($names);
-    $name = $names[$name_key];
-    unset ($names[$name_key]);
-
-    $photo_key = array_rand($photos);
-    $photo = $photos[$photo_key];
-    unset($photos[$photo_key]);
-
-    $last_name_key = array_rand($last_names);
-    $last_name = $last_names[$last_name_key];
-    unset ($last_names[$last_name]);
-
-    $wins = rand(0, 10);
-    $lost = rand(0, 10);
-
-    $stats = "W/L: $wins/$lost";
-    $win_percentage = $wins/($wins + $lost) * 100;
-
-    $cards[] = [
-        'full_name' => $name . $last_name ,
-        'photo' => $photo,
-        'stats' => $stats,
-        'win_percentage'=> round($win_percentage) .'%'
+    $products = [
+            [
+                'name' => 'Stumbro degtinė',
+                'price' => '6.49',
+                'image' => '...'
+            ],
+            [
+                'name' => 'Balzamas',
+                'price' => '9.50',
+                'price_special' => '7.99',
+                'image' => '...'
+            ]
     ];
-}
-
 
 ?>
 
@@ -43,10 +22,9 @@ for ($i = 0; $i < 6; $i++) {
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/main.css">
-    <title>Team</title>
+    <title>Prekės</title>
 </head>
 <body>
-<?php include 'templates/nav.php'; ?>
 <main>
     <div class="wrapper">
         <div class="cards">
@@ -55,7 +33,7 @@ for ($i = 0; $i < 6; $i++) {
                     <img src="<?php print $card['photo']; ?>">
                     <span> <?php print $card['stats']; ?></span>
                     <span><?php print $card['full_name']; ?></span>
-                    <span><?php print $card['win_percentage']; ?> </span>
+                    <span><?php print $card['price_special']; ?> </span>
                 </div>
             <?php endforeach; ?>
         </div>
