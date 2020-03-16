f752741dbbbf5e2c5f0a2c4b9aa38df121a966ac
Pilkapilka <juste.et.k@gmail.com>
2020-03-16 12:02:38 +0200

    klases darba soldier 03 16

--- index.php
+++ index.php
@@ -1,56 +1,47 @@
 <?php
-$h1 = 'Drink Catalogue';
-$price_display =  '';
+$h1 = 'War';
 
-        $cards = [
-            [
-                'name' => 'Stumbro degtinė',
-                'price' => 6.49,
+$fights = [
+    'soldiers'[
+        'my_soldier'=
+        'hedge'=
+            ['enemy_soldier'=]
 
-                'image' =>'css/degtinė.jpg'
-            ],
-            [
-                'name' => 'Balzamas',
-                'price' =>  9.50,
-                'price_special' => 8.00,
-                'image' => 'css/balzamas.jpg'
-            ],
-            [
-                'name' => 'Carlsberg alus',
-                'price' => 2.50,
-                'price_special' => 1.90,
-                'image' => 'css/carlsberg.jpg'
-            ],
-            [
-                'name' => 'Chardonay vynas',
-                'price' => 11.00,
-                'price_special' => 10.50,
-                'image' => 'css/chardonnay.jpg'
-            ]
-    ];
 
-        print $cards[0]['price_special'];
 
-    foreach($cards as $key => $card){
+];
 
-        if(isset($card['price_special'])){
-            $discount = round(100 - $card['price_special']  / $card['price'] * 100) . ' &euro';
+//print $cards[0]['price_special'];
 
-            $cards[$key]['discount'] = $discount;
+foreach ($cards as $key => $card) {
 
-            $cards[$key]['price_display'] = $card['price_special'];
+    if (isset($card['price_special'])) {
+        $discount = round(100 - $card['price_special'] / $card['price'] * 100) . ' eur';
+        $cards[$key]['discount'] = $discount;
 
-            var_dump($discount);
-        }
+        $cards[$key]['price_display'] = $card['price_special'];
 
-        else{
-            $cards[$key]['price_display'] = $card['price'];
-        }
+        var_dump($discount);
 
+    } else {
+        $cards[$key]['price_display'] = $card['price'];
     }
-var_dump($cards);
 
 
+    if ($card['instock'] > 0) {
+
+        $cards[$key]['instock_text'] = 'Yra sandėlyje';
+
+    } else {
+        $cards[$key]['instock_text'] = 'Nėra sandėlyje';
+    }
+
+    if($card['instock']  < 0) {
+
+        $cards[$key]['instock_class'] = '...';
+    }
+
+}
 
 ?>
 
@@ -63,22 +54,17 @@ var_dump($cards);
 </head>
 <body>
 <main>
-    <h1><?php $h1; ?></h1>
-    <div class="wrapper">
-        <div class="cards">
-            <?php foreach ($cards as $card): ?>
-                <div class="card">
-                    <?php if (isset($card['discount'])): ?>
-                        <span><?php print $card['discount']; ?> </span>
-                    <?php endif ; ?>
-
-                    <span><?php print $card['price_display']; ?> </span>
-
-                    <img src="<?php print $card['image']; ?>">
-                    <span><?php print $card['name']; ?></span>
-                </div>
-            <?php endforeach; ?>
-        </div>
+    <h1><?php print $h1; ?></h1>
+    <h2><?php print $h2; ?></h2>
+    <div class="fights-container">
+        <?php foreach ($fights as $fight): ?>
+            <div class="fight">
+                <div class="my_soldier">M</div>
+                <div class="hedge">-</div>
+                <?php for($i=0; $i < $fight['enemies_down']; $i++): ?>
+                    <div class="enemy_soldier">E</div>
+                <?php endfor; ?>
+            </div>
     </div>
 </main>
 </body>
