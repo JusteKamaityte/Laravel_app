a1d2b57b85049c74bff8be31e73dc2beee7ac1fb
Pilkapilka <juste.et.k@gmail.com>
2020-03-17 13:22:40 +0200

    funkcija funkcijoje komentarai

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$x = rand(1, 100);
+
 
 function pirminis($x){
     for($i = 2; $i <= $x / 2; $i++){
@@ -10,9 +10,37 @@ function pirminis($x){
          return true;
 }
 
-if($display == 1){
-    $
-}
+/**
+ * @param $x pirmas skaicius
+ * @param $y antras skaicius
+ * @return mixed
+ */
+    function sum($x, $y){
+        $sum = $x + $y;
+        return $sum;
+    }
+
+/**
+ * @param $x pirmas skaicius
+ * @param $y antras skaicius
+ * @return bool|mixed
+ */
+
+    function sum_if_prime($x, $y) {
+        if(pirminis($x) && pirminis($y)){
+            return sum(pirminis($x), $y);
+        }else {
+            return false;
+        }
+    }
+
+$x= rand(1, 100);
+$y = rand(1, 100);
+$sum = sum_if_prime($x, $y);
+
+$p1 = pirminis($x) ? "{$x} yra pirminis skaicius" : "{$x} nera pirminis skaicius";
+$p2 = pirminis($y) ? "{$y} yra pirminis skaicius" : "{$y} nera pirminis skaicius";
+$p3 = $sum ? "Pirm. sk. suma: {$sum}" : "--";
 ?>
 
 
@@ -20,10 +48,11 @@ if($display == 1){
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/main.css">
-    <title><?php print $title; ?></title>
+    <title>Pirminiai skaiciai</title>
 </head>
 <body>
-<h1><?php if(pirminis(x:7) ? 'Pirminis' : 'Nera pirminis' ;?></h1>
-
+<p><?php print $p1 ?></p>
+<p><?php print $p2 ?></p>
+<p><?php print $p3 ?></p>
 </body>
 </html>
