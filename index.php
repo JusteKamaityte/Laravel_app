14e1042f441624d3c1fb1775729e7ab63a9bcd90
Pilkapilka <juste.et.k@gmail.com>
2020-03-17 10:22:20 +0200

    police report task

--- index.php
+++ index.php
@@ -1,37 +1,43 @@
 <?php
+$police_reports = [
+        [
+                'subject' => 'Domantas',
+                'reason' => 'Public urination',
+                'amount' => 50
+        ],
+        [
+            'subject' => 'Migle',
+            'reason' => 'Drunk in public',
+            'amount' => 0
+        ],
+        [
+            'subject' => 'Juste',
+            'reason' => 'Skinny dipping',
+            'amount' => 0
+        ],
+        [
+            'subject' => 'Saulius',
+            'reason' => 'Constant complaining',
+            'amount' => 200
+        ]
+];
 
-$h2 = 'Enemy war';
-
-$my_soldier = 10;
-$enemy_soldier = rand(5, 15);
-$win_percentage = round($my_soldier / ($my_soldier + $enemy_soldier), 1)* 100;
-$win_chance = rand(0,100);
-
-$h1 = "Fight $my_soldier  $enemy_soldier";
-$h2 = 'enemies_killed';
-
-$fights = [];
-
-for ($i = 0; $i < $my_soldier; $i++) {
-    for($b=0; $b<$enemy_soldier; $b++){
-        var_dump("my_soldier : {$i} kovoja su priesu: {$b}");
+foreach($police_reports as $index => $report) {
+    if ($report['amount'] === 0) {
+        $report['warning'] = true;
+    } else {
+        $report['warning'] = false;
     }
-}
-//    $fights[$i]['enemies_killed'] = 0;
-//    while($enemy_soldier){
-//        var_dump("Cia yra enemies: {$enemy_soldier}");
-//        $rand = rand(0, 100);
-//        if ($win_chance >= $rand) {
-//            $fights[$i]['enemies_killed'] += 1;
-//            $enemy_soldier--;
-//        } else {
-//            break;
-//        }
-//}
-//
-//foreach($fights as $fight){
-//    print"{$fight['enemies_killed']}";
-//}
+    $report['text'] = $report['subject'] . '(' . $report['reason'] .')';
+    if($report['warning']){
+        $report['text'] .= 'Ispejimas';
+    }else{
+        $report['text'] .= 'Bauda' .$report['amount'];
+    }
+    $police_reports[$index] = $report;
+    }
+    var_dump($police_reports);
+    $h1 = 'Palicijos israsas';
 ?>
 
 
@@ -39,25 +45,16 @@ for ($i = 0; $i < $my_soldier; $i++) {
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/main.css">
-    <title>Fight</title>
+    <title><?php print $title ;?></title>
 </head>
 <body>
-<main>
-    <h1><?php print $h1; ?></h1>
-    <h2><?php print $h2; ?></h2>
-    <div class="fights-container">
-        <?php foreach ($fights as $fight): ?>
-             <div class="fight">
-                <div class="my_soldier">M</div>
-                <div class="hedge">-</div>
-                <?php for ($i = 0;
-                $i < $fight['enemies_killed'];
-                $i++): ?>
-                    <div class="enemy_soldier">E</div>
-            <?php endfor; ?>
-        </div>
-        <?php endforeach; ?>
-    </div>
-</main>
+    <h1><?php print $h1 ; ?></h1>
+        <ul>
+            <?php foreach ($police_reports as $index => $report): ?>
+            <li>
+                <?php print $report['text']; ?>
+            </li>
+            <?php endforeach ; ?>
+        </ul>
 </body>
 </html>
