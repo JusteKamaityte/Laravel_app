7417f5af39dd821a919632e587a260c5508cb8a7
Pilkapilka <juste.et.k@gmail.com>
2020-03-19 10:32:10 +0200

    panos

--- index.php
+++ index.php
@@ -1,98 +1,56 @@
 <?php
-
+$panos = [
+    [
+        'vardas' => 'Monika',
+        'graži' => true,
+        'protinga' => false
+    ],
+    [
+        'vardas' => 'Julija',
+        'graži' => false,
+        'protinga' => false
+    ],
+    [
+        'vardas' => 'Aistė',
+        'graži' => false,
+        'protinga' => true
+    ],
+    [
+        'vardas' => 'Lina',
+        'graži' => true,
+        'protinga' => true
+    ],
+];
 /**
- * funkcija, generuojanti kvadratinę matricą
- *
- * @param $size
+ * @param $panos
  * @return array
  */
-function generate_matrix($size)
+function normali_pana(array $panos): array
 {
-    $matrix = [];
-
-    for ($x = 0; $x < $size; $x++) {
-        for ($y = 0; $y < $size; $y++) {
-            $matrix[$x][$y] = rand(0, 1);
+    $results = [];
+    foreach ($panos as $pana) {
+        if ($pana['graži'] && $pana['protinga']) {
+            $results[] = $pana;
         }
     }
-
-    return $matrix;
-}
-/**
-* @param $matrix
- */
-function get_winning_rows(array $matrix): ?array
-{
-
-    $winners = [];
-
-    foreach($matrix as $row_key=> $row){
-        $comparison = $row[0];
-        $is_winner = true;
-
-        foreach ($row as $col) {
-            if ($col != $comparison) {
-                $is_winner = false;
-                break;
-            }
-        }
-
-        // Jeigu eilutė laiminga, įtraukiame
-        // jos raktą į laimingų eilučių masyvą
-        if ($is_winner) {
-            $winners[] = $row_key;
-        }
-    }
-
-    return empty(!$winners) ? $winners : null;
+    return $results;
 }
 
-$array = generate_matrix(3);
-$winners = get_winning_rows($array);
-
+$normali = normali_pana($panos);
+$result = 'Graži ir protinga: ';
 ?>
 
 <html>
 <head>
-    <title>Matrica</title>
+    <title>Panos</title>
     <style>
-        .matrix {
-            width: 350px;
-        }
 
-        .row {
-            display: flex;
-            flex-direction: row;
-        }
-
-        .row div {
-            height: 100px;
-            width: 100px;
-            margin: 10px;
-        }
-
-        .blue {
-            background-color: blue;
-        }
-
-        .green{
-            background-color: green;
-        }
-
-        .win {
-            border: solid 2px red;
-        }
     </style>
 </head>
 <body>
-<div class="matrix">
-    <?php foreach ($array as $key => $row): ?>
-        <div class="row <?php print in_array($key, $winners) ? 'win' : ''; ?>">
-            <?php foreach ($row as $item): ?>
-                <div class="<?php print $item ? 'blue' : 'green'; ?>"></div>
-            <?php endforeach; ?>
-        </div>
-    <?php endforeach; ?>
-</div>
+<ul>
+    <?php foreach($normali as $pana): ?>
+        <li><?php print $result;?><?php print $pana['vardas']?></li>
+</ul>
 </body>
 </html>
