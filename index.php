4b7a66887dfbc45669c82fee4b38ef548eaedf8b
Pilkapilka <juste.et.k@gmail.com>
2020-03-23 11:51:30 +0200

    slider submit and get image size

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <?php
-/**
- *funkcija skaičiuojanti kvadratini laipsnį
- * @param $x
- * @return float|int
- */
-function square(float $x): float
-{
+$default_size = 300;
+$object_size = $default_size;
+//
+//function size(float $x): float{
+//
+//}
+//
+//$atsakymas = ''
 
-    return $x ** 2;
-}
+if (isset($_POST['object'])) {
+    $object_size = $default_size * (($_POST['object'] / 100) * 2);
 
-$h2 = 'please enter a number';
-
-if (isset($_POST['number'])) {
-    $skaicius = $_POST['number'];
-
-    $square_function = square($skaicius);
-
-} else {
-    $square_function = 'įveskite skaičių';
 }
 
 
@@ -29,15 +21,28 @@ var_dump($_POST);
 <head>
     <title>&</title>
     <style>
+        .object_1, .object_2{
+            display: inline-block;
+            border-radius: 50%;
+            background: salmon;
+        }
+
 
     </style>
 </head>
 <body>
 <form method="POST">
-    <label for="num">Ką pakelti kvadratu: </label>
-    <input id="num" name="number" type="number" min="1"  max="200" required/>
-    <button name="action" value="square" >Kelti kvadratu</button>
+    <label>
+        <input name="object"  type="range" min="1" max="100">
+    </label>
+    <button name="action" >Pasirinkite dydį</button>
 </form>
-    <h2> <?php print $square_function ?></h2>
+
+<div class="object" >
+    <div class="object_2"  style="height:<?php print $object_size; ?>;px; width:<?php print $object_size; ?>;px;"> </div>
+    <div class="object_1"  style="height:<?php print $object_size; ?>;px; width:<?php print $object_size; ?>;px;"> </div>
+
+</div>
+
 </body>
 </html>
