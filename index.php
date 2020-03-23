888ef752b3f4763aac8a8143bdad2bf61b4362c8
Pilkapilka <juste.et.k@gmail.com>
2020-03-23 09:02:38 +0200

    repeating 0320 tasks with &

--- index.php
+++ index.php
@@ -1,46 +1,11 @@
 <?php
-
-/**
- *funkcija pakeicia recursivearray reiksmes
- * @param $array
- * @param $from
- * @param $to
- */
-function recursive_replace(&$array, $from, $to){
-    if(is_array($array)) {
-        if (!isset($array[''])) {
-            $array[''] = TRUE;
-            foreach($array as $key=>$val) {
-                if(is_array($array[$key]) || is_object($array[$key])) {
-                    recursive_replace($array[$key], $find, $replace);
-                }
-                else{
-                    if($array[$key] === $find) {
-                        $array[$key] = $replace;
-                    }
-                }
-            }
-            unset($data[RECURSIVE_REPLACE_MARKER]);
-        }
-    }
-    elseif (is_object($data)) {
-        if (!isset($data->RECURSIVE_REPLACE_MARKER)) {
-            $data->RECURSIVE_REPLACE_MARKER = TRUE;
-            foreach($data as $key=>$val) {
-                if(is_array($data->$key) || is_object($data->$key)){
-                    recursive_replace($data->$key, $find, $replace);
-                }else{
-                    if($data->$key === $find) {
-                        $data->$key = $replace;
-                    }
-                }
-            }
-            unset($data->RECURSIVE_REPLACE_MARKER);
-        }
-    }
+$x = 0;
+function change_x(&$x){
+    $x = 1;
 }
+change_x($x);
 
-var_dump(recursive_replace($data));
+var_dump($x);
 
 ?>
 
