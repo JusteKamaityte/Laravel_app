d5c1795f3ee20cad393b750af19cabbb3aa96d07
Pilkapilka <juste.et.k@gmail.com>
2020-03-25 10:24:48 +0200

    form template

--- index.php
+++ index.php
@@ -11,23 +11,75 @@ function html_attr(array $attr): string
     foreach ($attr as $index => $value) {
         $attributes .= "$index=\"$value\" ";
 
-
     }
     return $attributes;
 }
 
+
 $form = [
     'attr' => [
         'action' => 'index.php',
-        'method' => 'POST',
         'class' => 'my-form',
+        'id' => 'form'
+    ],
+    'fields' => [
+        'first_name' => [
+            'label' => 'First name',
+            'type' => 'text',
+            'value' => 'Petras',
+            'extra' => [
+                'attr' => [
+                    'jj' => 'dasa'
+                ]
+            ]
+        ],
+        'last_name' => [
+            'label' => 'Last name',
+            'type' => 'text',
+
+        ],
+        'email' => [
+            'label' => 'Email',
+            'type' => 'email',
+            'value' => '...',
+        ],
+        'password' => [
+            'label' => 'Password',
+            'type' => 'password',
+            'value' => '...',
+        ],
+        'select' => [
+            'label' => 'selected',
+            'type' => 'select',
+            'value' => '',
+            'options' => [
+                'option_one' => 'first',
+                'option_two' => 'second',
+                'option_three' => 'third'
+            ],
+            'extra' => [
+                'attr' => [
+                    'class' => 'form_class',
+                    'id' => 'form_test_id'
+                ]
+            ]
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'register',
+            'name' => 'action',
+            'extra' => [
+                'attr' => [
+                    'class' => 'submit-button'
+                ]
+            ]
+
+        ],
     ]
 ];
 
 
-
-var_dump(html_attr($form['attr']));
-var_dump($form);
 ?>
 
 <html>
@@ -37,6 +89,11 @@ var_dump($form);
 
 </head>
 <body>
+<form method="POST">
+    <input type="text" name="login" placeholder="Name">
+    <input type="password" name="password" placeholder="password">
+    <input type="submit">
+</form>
 <?php include 'templates/form.tpl.php'; ?>
 </body>
 </html>
--- templates/form.tpl.php
+++ templates/form.tpl.php
@@ -1,13 +1,41 @@
-
-
-<form <?php print html_attr($form['attr'] ?? []) + ['method'=> 'POST']); ?>>
-
+<form
+    <?php print html_attr(($form['attr'] ?? []) + ['method' => 'POST']); ?>>
     <?php foreach ($form['fields'] ?? [] as $field_id => $field): ?>
+        <label><span><?php print $field['label']; ?></span> </label>
+        <?php if (in_array($field['type'], ['text', 'password', 'email', 'number'])): ?>
+        <?php var_dump($field); ?>
+            <input <?php
+            print html_attr(
+                ($field['extra']['attr'] ?? []) +
+                [
+                    'name' => $field_id,
+                    'type' => $field['type'],
+                    'value' => $field['value'] ?? ''
+                ]); ?>>
+        <?php elseif (in_array($field['type'], ['select'])): ?>
+            <select <?php print html_attr(($form['attr'] ?? [])); ?>>
+                <?php foreach ($field['options'] as $option_id => $option): ?>
+                    <option value="<?php print $option_id; ?>">
+                        <?php print ($field['value'] == $option_id) ? 'selected' : ''; ?>
+                        <?php print $option; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        <?php elseif (in_array($field['type'], ['textarea'])) : ?>
+            <textarea <?php print html_attr(($field['extra']['attr'] ?? []) +
+                [
+                    'name' => $field_id,
+                ]); ?>>
+                <?php print $field['value'] ?? ''; ?>
+            </textarea>
+        <?php endif; ?>
 
     <?php endforeach; ?>
-
     <?php foreach ($form['buttons'] ?? [] as $button_id => $button): ?>
-
+        <button <?php print html_attr(
+            ($button['extra']['attr'] ?? []) + ['value' => $button_id, 'name' => 'action']); ?>>
+        <?php print $button['text']; ?>
+        </button>
     <?php endforeach; ?>
 </form>
 
