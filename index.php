c330b258dfb8f77397bda029c6cbdf785116e8a8
Pilkapilka <juste.et.k@gmail.com>
2020-03-26 10:57:43 +0200

    filter  masyva

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 //    }
 //    return $attr;
 //}
-//
-//
+
+
 //    foreach ($attr as $index => $value) {
 //        $attributes .= "$index=\"$value\" ";
 //
@@ -26,15 +26,15 @@
 
 /**
  * funkcija apsauganti nuo pavojingu ivesciu(POST)
- * @param $fields
+ * @param array $form
  * @return array|null
  */
-function get_filtered_input(array $fields): ?array
+function get_filtered_input(array $form): ?array
 {
     //parametrai pagal kuriuos nusako kaip reikia POST masyva isfiltruoti
     $filter_parameters = [];
 
-    foreach ($fields as $field_id => $field_value) {
+    foreach ($form['fields'] as $field_id => $field_value) {
         $filter_parameters[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
         //filter konstanta turi savo verte ir ji supranta tik tai savo verte, todel kitur nei funkcijos viduj negali buti parasyta
     }
@@ -45,74 +45,74 @@ function get_filtered_input(array $fields): ?array
 
 
 
-//$form = [
-//    'attr' => [
-//        'action' => 'index.php',
-//        'method' => 'POST',
-//        'class' => 'my-form',
-//        'id' => 'form'
-//    ],
-//    'fields' => [
-//        'first_name' => [
-//            'label' => 'First name',
-//            'type' => 'text',
-//            'value' => 'Petras',
-//            'extra' => [
-//                'attr' => [
-//                    'jj' => 'dasa'
-//                ]
-//            ]
-//        ],
-//        'last_name' => [
-//            'label' => 'Last name',
-//            'type' => 'text',
-//
-//        ],
-//        'email' => [
-//            'label' => 'Email',
-//            'type' => 'email',
-//            'value' => '...',
-//        ],
-//        'password' => [
-//            'label' => 'Password',
-//            'type' => 'password',
-//            'value' => '...',
-//        ],
-//        'select' => [
-//            'label' => 'selected',
-//            'type' => 'select',
-//            'value' => '',
-//            'options' => [
-//                'option_one' => 'first',
-//                'option_two' => 'second',
-//                'option_three' => 'third'
-//            ],
-//            'extra' => [
-//                'attr' => [
-//                    'class' => 'form_class',
-//                    'id' => 'form_test_id'
-//                ]
-//            ]
-//        ]
-//    ],
-//    'buttons' => [
-//        'submit' => [
-//            'text' => 'register',
-//            'name' => 'action',
-//            'extra' => [
-//                'attr' => [
-//                    'class' => 'submit-button'
-//                ]
-//            ]
-//
-//        ],
-//    ]
-//];
+$form = [
+    'attr' => [
+        'action' => 'index.php',
+        'method' => 'POST',
+        'class' => 'my-form',
+        'id' => 'form'
+    ],
+    'fields' => [
+        'first_name' => [
+            'label' => 'First name',
+            'type' => 'text',
+            'value' => 'Petras',
+            'extra' => [
+                'attr' => [
+                    'jj' => 'dasa'
+                ]
+            ]
+        ],
+        'last_name' => [
+            'label' => 'Last name',
+            'type' => 'text',
+
+        ],
+        'email' => [
+            'label' => 'Email',
+            'type' => 'email',
+            'value' => '...',
+        ],
+        'password' => [
+            'label' => 'Password',
+            'type' => 'password',
+            'value' => '...',
+        ],
+        'select' => [
+            'label' => 'selected',
+            'type' => 'select',
+            'value' => '',
+            'options' => [
+                'option_one' => 'first',
+                'option_two' => 'second',
+                'option_three' => 'third'
+            ],
+            'extra' => [
+                'attr' => [
+                    'class' => 'form_class',
+                    'id' => 'form_test_id'
+                ]
+            ]
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'register',
+            'name' => 'action',
+            'extra' => [
+                'attr' => [
+                    'class' => 'submit-button'
+                ]
+            ]
+
+        ],
+    ]
+];
 
 
 
-var_dump(get_filtered_input($_POST));
-$safe_input = get_filtered_input($_POST);
+var_dump($form);
+$safe_input = get_filtered_input($form);
 ?>
 
 <html>
@@ -124,6 +124,6 @@ $safe_input = get_filtered_input($_POST);
 <body>
 <h1><?php print $safe_input['vardas'] ?? ''; ?></h1>
 <h2>Hack it</h2>
-    <?php include 'templates/form.tpl.php'; ?>
+<!--    --><?php //include 'templates/form.tpl.php'; ?>
 </body>
 </html>
